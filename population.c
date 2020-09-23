5ec92029637f8fb08a3b870ddcb6d1f991fd0b4a
1000-mikhail <71731449+1000-mikhail@users.noreply.github.com>
2020-09-23 19:20:26 +0300

    Lab 1: Population Growth
    Lab 1: Population Growth

---
+++ population.c
@@ -0,0 +1,16 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    
+for (int row = 0; row < 3; row++)
+{
+     for (int column = 0; column < 4; column++)
+     {
+        printf("x");
+     }
+        printf("\n");
+}
+
+}
